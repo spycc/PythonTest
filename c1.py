a33c231c485ce20ec0cefafa66ed171d8cf7453d
spycc <572014@qq.com>
2018-01-21 23:06:38 +0800

    1/21password

---
+++ c1.py
@@ -0,0 +1,16 @@
+# account = 'spycc'
+# password = '111111'
+
+# user_account = input('please account:')
+# user_password = input('please password:')
+
+# if user_account == account and user_password == password:
+#     print('you are login ok')
+# else:
+#     print('Sorry wrong!!!')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
