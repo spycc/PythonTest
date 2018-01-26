0f90dc3eda89f5c211619841aa73b6265facd617
spycc <572014@qq.com>
2018-01-26 17:38:40 +0800

    1/26
    正则表达式练习

--- mod1/a2.py
+++ mod1/a2.py
@@ -1,1 +1,8 @@
-print('file name:'+__name__)
+a = 'abbcccddddeeeeeffffff'
+print('a' in a)
+#print(a.index('a'))
+print(a.index('b'))
+print(a.index('c'))
+print(a.index('d'))
+#print(a.index('e'))
+#print(a.index('f'))
--- mod2/b1.py
+++ mod2/b1.py
@@ -1,1 +1,9 @@
-print('file name:' + __name__)
+import re
+
+a = 'hello,good,all,to,of,count,aac,abc,acc,_adc,aec,afc'
+b = 'pythonpythonpythonjsadfwfee'
+c = '10000b0011b'
+d = 'goodC#haha'
+
+count = re.findall('c#',d,re.I)
+print(count)
